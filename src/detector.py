1e302a9f1a9904e64c4d134be2badcfb680723fa
Hans Georg Schaathun <georg@schaathun.net>
2021-11-22 19:03:31 +0100

    Working version of the detector

--- src/detector.py
+++ src/detector.py
@@ -1,12 +1,19 @@
 #! /usr/bin/env python
 """Demo of a feature detection.
 
-NB: Untested, unfinished!"""
+It loads a test image and draws red circles 
+around the feature points.  The result is
+saved to a file in the current directory.
+"""
 
 import cv2 as cv
 from tracking import *
 
 img = cv.imread( "../data/frame28.jpeg")
-img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-getHarris(img)
+cornerlist = getHarris(grey)
+
+for (pos,s) in cornerlist:
+    cv.circle(img,pos,10,(0,0,255))
+cv.imwrite( "features.jpeg", img)
