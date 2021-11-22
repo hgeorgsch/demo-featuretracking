6081c78d0f71ba5fc1b31329ea55bdbf1481dd8c
Hans Georg Schaathun <georg@schaathun.net>
2021-11-22 14:49:19 +0100

    Enforce grey scale on loaded images.

--- src/tracker.py
+++ src/tracker.py
@@ -8,6 +8,8 @@ from tracking import *
 
 img1 = cv.imread( "../data/frame27.jpeg")
 img2 = cv.imread( "../data/frame28.jpeg")
+img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
 
 optical_flow(img1, img2, numpts=5, debug=10)
